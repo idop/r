3cbafc07499b33b2b3b469f408f2e1a644ac9229
Michael Kreimer <michaelkreimer@gmail.com>
2016-04-25 20:18:33 +0300

    test

---
+++ B16 Ex02 MichaelKreimer 305597478 IdoPerry 036928646/Ex02_GameUtils/TreeNode.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ex02_GameUtils
+{
+    public class TreeNode<T> : IEnumerable<TreeNode<T>>
+    {
+
+        public T Data { get; set; }
+        public TreeNode<T> Parent { get; set; }
+        public ICollection<TreeNode<T>> Children { get; set; }
+
+        public TreeNode(T data)
+        {
+            this.Data = data;
+            this.Children = new LinkedList<TreeNode<T>>();
+        }
+
+        public TreeNode<T> AddChild(T child)
+        {
+            TreeNode<T> childNode = new TreeNode<T>(child) { Parent = this };
+            this.Children.Add(childNode);
+            return childNode;
+        }
+
+        public IEnumerator<TreeNode<T>> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
